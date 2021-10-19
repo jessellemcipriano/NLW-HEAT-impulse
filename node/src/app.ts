839aa8d089802ec50c0e176926163a6704c53df6
Jéssellem S. Cipriano <69681532+jessellemcipriano@users.noreply.github.com>
2021-10-19 00:00:07 -0300

    aula 01 - nlw

---
+++ node/src/app.ts
@@ -0,0 +1,22 @@
+import "dotenv/config"
+import express from "express";
+import { router } from "./routes";
+
+const app = express();
+app.use(express.json())
+app.use(router);
+
+app.get("/github",(request, response) => {
+    response.redirect(`https://github.com/login/oauth/authorize?client_id=${process.env.GITHUB_CLIENT_ID}`)
+})
+
+app.get("/signin/callback",(request, response) => {
+    const {code} = request.query;
+    return response.json(code);
+})
+
+app.get("/teste",(request, response) => {
+    console.log("to aqi")
+})
+
+app.listen(3030, () => console.log('ouvindo na Porta 3030'))
